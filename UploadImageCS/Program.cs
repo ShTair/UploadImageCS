e031384370306c38a413f3ee5eb6fe75c88012b2
ShTair <a@vsh.jp>
2017-05-26 16:00:01 +0900

    アップロードできるまで

--- UploadImageCS/Program.cs
+++ UploadImageCS/Program.cs
@@ -1,8 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
 
 namespace UploadImageCS
 {
@@ -10,6 +10,47 @@ namespace UploadImageCS
     {
         static void Main(string[] args)
         {
+            using (var stream = File.OpenRead(@"アップロードするファイルパス"))
+            {
+                var fileName = DateTime.Now.ToString("yyyyMMddHHmmss");
+
+                var content = new MultipartFormDataContent();
+                //content.Add(new StreamContent(stream), "uploadfile", fileName);
+                ////パターンA↑　本来はこれでよい。
+                content.Add(CreateStreamContent(stream, fileName + ".jpg")); //←拡張子必須
+                content.Add(new StringContent(fileName), "title");
+
+                var handler = new HttpClientHandler
+                {
+                    Credentials = new NetworkCredential("ユーザ名", "パスワード"),
+                };
+
+                using (var hc = new HttpClient(handler))
+                {
+                    var reqContentString = content.ReadAsStringAsync().Result;
+
+                    var res = hc.PostAsync("http://cs.nagao.nuie.nagoya-u.ac.jp/upload/images", content).Result;
+                    var resContentString = res.Content.ReadAsStringAsync().Result;
+                }
+            }
+        }
+
+        private static StreamContent CreateStreamContent(Stream stream, string fileName)
+        {
+            var content = new StreamContent(stream);
+            //content.Headers.ContentDisposition = new ContentDispositionHeaderValue("form-data")
+            //{
+            //    Name = "uploadfile",
+            //    FileName = fileName,
+            //};
+            ////パターンA'↑　パターンAでうまくいかないときはこうする。ほぼパターンAと同じ意味。
+            ////しかしこれでもうまくいかないので
+            content.Headers.Add("Content-Disposition", "form-data; name=\"uploadfile\"; filename=\"" + fileName + "\"");
+            ////パターンB↑　ヘッダに直接書く。
+            ////パターンA'だと、form-data; name=uploadfile; filename=fileNameというヘッダになる。
+            ////なぜかダブルクオーテーション必須で、form-data; name="uploadfile"; filename="fileName"にしないといけない。
+            content.Headers.ContentType = new MediaTypeHeaderValue("image/jpeg");
+            return content;
         }
     }
 }
